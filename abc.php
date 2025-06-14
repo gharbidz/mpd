980d9b9d40ef8b7d6872915537cbbfad78c19c74
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-06-14 23:36:43 +0100

    Create abc.php

---
+++ abc.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 1",
+    "stream_url": "https://bestgooal.live/moviball/channel/mbcaction/index.php?file=index.mpd",
+    "drm_keys": {
+        "61500ce9a45342b897ba868f4fc59910": "93a1c516e6b4fc91fa88c59922f6d3d7"
+    }
+}
