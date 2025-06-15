1e245999636eb7fbe14de497c0fa11c5b1f90dd4
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-06-15 18:00:17 +0100

    Create absaction.php

---
+++ absaction.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 1",
+    "stream_url": "https://shls-mbcaction-prod-dub-ak.akamaized.net/out/v1/8732f5132b274000bd59aee64255241c/index.mpd",
+    "drm_keys": {
+        "61500ce9a45342b897ba868f4fc59910": "93a1c516e6b4fc91fa88c59922f6d3d7"
+    }
+}
