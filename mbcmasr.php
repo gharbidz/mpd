d3b761f705827e208324c53df06fdb6daa742749
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-07-01 00:04:59 +0100

    Create mbcmasr.php

---
+++ mbcmasr.php
@@ -0,0 +1,8 @@
+
+{
+    "title": "MBC MASR",
+    "stream_url": "https://uselector.cdn.intigral-ott.net/MBMS2/MBMS2.isml/manifest.mpd",
+    "drm_keys": {
+        "4da4fec209622c5da04762e8d2d2f1d7": "4495168cf1a99038546b9d042dbe7c97"
+    }
+}
