7f29234b407c22b5850faa965d6b6c4d8ef37082
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-07-15 01:23:10 +0100

    Create today.js

---
+++ today.js
@@ -0,0 +1,22 @@
+// matches.js
+document.getElementById("MatchesFlex").innerHTML = `
+<div class="EventBox">
+  <a href="#" title="تفاصيل المباراة">
+    <div class="EventCover"><div class="EventIcon"></div></div>
+  </a>
+  <div class="EventTeams">
+    <div class="EventTeam Right">
+      <div class="EventTeamLogo"><img src="..." /></div>
+      <div class="EventTeamName">أودا ريغا</div>
+    </div>
+    <div class="EventCenter">
+      <div class="EventTiming"><div>19:00</div><div>0 - 0</div></div>
+    </div>
+    <div class="EventTeam Left">
+      <div class="EventTeamLogo"><img src="..." /></div>
+      <div class="EventTeamName">لارن أف.سي.</div>
+    </div>
+  </div>
+  <ul class="EventFooter"><li>Unknown</li><li>Unknown</li><li>دوري المؤتمر الأوروبي - تصفيات - الجولة الأولى</li></ul>
+</div>
+`;
